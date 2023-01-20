1228e4f984babffa3e9c4b3a4d7388c1e9e7a529
Adrien <adrien.anodeau@gmail.com>
2023-01-20 17:09:48 +0100

    improve about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,21 +4,25 @@ import "./About.scss";
 
 const About = () => {
   return (
-    <div className="about_ctr">
-      <h1 className="about_title">About me</h1>
-      <p className="about_content">
-        I am a web and mobile developer focusing on front-end with full-stack
-        experiences.
-      </p>
-      <p className="about_content">
-        I am mainly working with JavaScript technologies such as React, React
-        Native, Vue, TypeScript.
-      </p>
-      <p className="about_content">
-        I am enthusiast about creating and developing clean and efficient
-        websites & web applications.
-      </p>
-      <Skills />
+    <div className="about_section">
+      <div className="about_ctr">
+        <h1 className="about_title">about me</h1>
+        <p className="about_content">
+          I am a web and mobile developer focusing on front-end with full-stack
+          experiences.
+        </p>
+        <p className="about_content">
+          I am mainly working with JavaScript technologies such as React, React
+          Native, Vue, TypeScript.
+        </p>
+        <p className="about_content">
+          I am enthusiast about creating and developing clean and efficient
+          websites & web applications.
+        </p>
+      </div>
+      <div className="skills_ctr">
+        <Skills />
+      </div>
     </div>
   );
 };
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,25 +1,38 @@
 import React from "react";
 import "./Skills.scss";
+import { techBack, techDb, techFront, techVersioning } from "../../utils/tech";
 
 const Skills = () => {
+  function createTechItem(techArray) {
+    return techArray.map((t, i) => (
+      <div className="tech_item" key={t.tech}>
+        <span>{t.tech}</span>
+        <img src={t.icon} alt={t.icon} className="tech_icon" />
+      </div>
+    ));
+  }
+
+  const frontArray = createTechItem(techFront);
+  const backArray = createTechItem(techBack);
+  const dbArray = createTechItem(techDb);
+  const vArray = createTechItem(techVersioning);
+
   return (
-    <div>
-      <h2 className="skills_title">Stack I am currently working with:</h2>
-      <ul className="skills">
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>React Native</li>
-        <li>TypeScript</li>
-        <li>Vue</li>
-        <li>CSS</li>
-        <li>HTML</li>
-        <li>Express</li>
-        <li>PHP</li>
-        <li>Laravel</li>
-        <li>SQL</li>
-        <li>MongoDB</li>
-      </ul>
-    </div>
+    <>
+      <h2 className="skills_title">technologies I work with:</h2>
+
+      <h3>front-end:</h3>
+      <div className="tech_ctr">{frontArray}</div>
+
+      <h3>back-end:</h3>
+      <div className="tech_ctr">{backArray}</div>
+
+      <h3>databases:</h3>
+      <div className="tech_ctr">{dbArray}</div>
+
+      <h3>versioning:</h3>
+      <div className="tech_ctr">{vArray}</div>
+    </>
   );
 };
 
