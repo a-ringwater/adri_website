ccbf8dceff7bf2128e3d9d8f180d959d0bbd9c8a
Adrien <adrien.anodeau@gmail.com>
2023-02-03 16:23:42 +0100

    apply dark/light mode on button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { fonts, colors } from "../assets/styleVar";
+import { ThemeContext } from "../context/ThemeContext";
 
-const Button = (props) => {
-  const { children, className } = props;
+const ButtonStyled = styled.button`
+  font-family: ${fonts.primary};
+  font-size: 20px;
+  background-color: transparent;
+  border: solid 1px
+    ${(props) =>
+      props.theme !== "light_mode" ? colors.green_variant : colors.navy};
+  border-radius: 0.25em;
+  color: ${(props) =>
+    props.theme !== "light_mode" ? colors.green_variant : colors.navy};
+  padding: 1em;
+  min-width: 100px;
+  transition: transform 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const Button = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <button type="button" className={className}>
+    <ButtonStyled type="button" theme={theme}>
       {children}
-    </button>
+    </ButtonStyled>
   );
 };
 
